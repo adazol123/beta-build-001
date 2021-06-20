29d8773f46d067baa16633af75f945fd614f8804
Daniel Lozada <dpadayao40@gmail.com>
2021-06-20 01:58:53 -0400

    change image on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const About = () => {
     return (
         <div className='flex-center-column h-md py-md text-center'>
-            <img style={{marginTop:'72px'}} src={`https://firebasestorage.googleapis.com/v0/b/djlozada.appspot.com/o/person-black.png?alt=media&token=169b7881-867e-40ef-8b3d-f58d4b2704c9`} alt="profile" />
+            <img style={{marginTop:'72px'}} src={`https://firebasestorage.googleapis.com/v0/b/djlozada.appspot.com/o/person-black.png?alt=media&token=7c4e90c3-ed0d-4c33-b5c9-2762c14e80f0`} alt="profile" />
             <h1 >Wanna know about me? 🐶 </h1>
         </div>
     )
